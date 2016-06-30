2afe498c02d14243d2d92e5b20d19e54420daf7b
Christian Hubinger <christian@trigo.at>
2016-06-30 23:32:34 +0200

    auto-nvm module

---
+++ modules/auto-nvm/module.zsh
@@ -0,0 +1,15 @@
+# place this after nvm initialization!
+
+# echo "Loading auto-nvm..."
+source $(brew --prefix nvm)/nvm.sh
+autoload -U add-zsh-hook
+load-nvmrc() {
+	if [[ -f .nvmrc && -r .nvmrc ]]; then
+		nvm use
+	# elif [[ $(nvm version) != $(nvm version default)  ]]; then
+		# echo "Reverting to nvm default version"
+		# nvm use default
+	fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
